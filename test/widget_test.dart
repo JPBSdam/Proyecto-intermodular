02f1cba38b660981624120f28915e102a40ba014
Raquel SG - 3592917 <3592917@alu.murciaeduca.es>
2026-03-02 13:52:33 +0100

    Se corrige formato del test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,11 +5,7 @@ void main() {
   // Test básico que verifica que el framework funciona
   testWidgets('Flutter test framework is working', (WidgetTester tester) async {
     await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: Text('Test'),
-        ),
-      ),
+      const MaterialApp(home: Scaffold(body: Text('Test'))),
     );
 
     expect(find.byType(Scaffold), findsOneWidget);
