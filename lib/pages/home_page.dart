c7cb06e00b80c0a201594b7fb34f4c416d6c4285
Maria Collado <3063940@alu.murciaeduca.es>
2026-03-04 17:43:27 +0100

    variables usuario y anonimmo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,6 +36,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final user = _authService.currentUser;
+    final isAnonymous = _authService.isAnonymous();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
