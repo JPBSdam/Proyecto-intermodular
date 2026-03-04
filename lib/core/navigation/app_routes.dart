6dfc701a686b8e94cb80c23905dfbadd40e0271b
Raquel SG - 3592917 <3592917@alu.murciaeduca.es>
2026-03-04 11:26:38 +0100

    Actualización de rutas definidas en AppRoutes

--- lib/core/navigation/app_routes.dart
+++ lib/core/navigation/app_routes.dart
@@ -1,6 +1,9 @@
 class AppRoutes {
   static const String home = '/home';
   static const String login = '/login';
+  static const String register = '/register';
   static const String menu = '/menu';
   static const String reservations = '/reservations';
+  static const String profile = '/profile';
+  static const String about = '/about';
 }
