092d09fe7f368940c3b9877d372f0f12ce940b1e
Raquel SG - 3592917 <3592917@alu.murciaeduca.es>
2026-03-09 22:13:15 +0100

    Actualizar .gitignore y limpiar tests de widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,61 +14,6 @@ void main() {
       expect(find.byType(Scaffold), findsOneWidget);
       expect(find.text('Test'), findsOneWidget);
     });
-
-    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-      // Test simple de contador sin Firebase
-      await tester.pumpWidget(MaterialApp(home: _TestCounterWidget()));
-
-      // Verificar que el contador comienza en 0
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
-      // Tap en el botón +
-      await tester.tap(find.byIcon(Icons.add));
-      await tester.pump();
-
-      // Verificar que el contador se incrementó a 1
-      expect(find.text('0'), findsNothing);
-      expect(find.text('1'), findsOneWidget);
-    });
   });
 }
 
-// Widget de test simple con contador
-class _TestCounterWidget extends StatefulWidget {
-  @override
-  _TestCounterWidgetState createState() => _TestCounterWidgetState();
-}
-
-class _TestCounterWidgetState extends State<_TestCounterWidget> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
