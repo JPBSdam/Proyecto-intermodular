746d0345a4e60ac1ac6390e4129c63829b4b7e2a
Jesús Pablo Bermejo Salar <2949625@alu.murciaeduca.es>
2026-03-23 09:48:12 +0100

    añade vista de lista de platos

---
+++ lib/ui/views/data/dish_list_view.dart
@@ -0,0 +1,69 @@
+import 'package:app_restaurante/core/navigation/app_routes.dart';
+import 'package:app_restaurante/core/widgets/home_button.dart';
+import 'package:app_restaurante/core/widgets/loading_overlay.dart';
+import 'package:app_restaurante/ui/viewmodels/firestore/dish_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class DishesListView extends StatelessWidget {
+  const DishesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final viewmodel = context.watch<DishViewModel>();
+
+    // Cargar platos solo una vez
+    if (!viewmodel.isWatchingDishes) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        viewmodel.watchDishes();
+      });
+    }
+
+    return LoadingOverlay(
+      isLoading: viewmodel.isLoading,
+      // tengo la impresión de que se podría crear una variable global para el loading
+      // tener un setter del value tambien global y llamarlo cada vez que haga falta
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Lista de Platos"),
+          actions: const [HomeButton()],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //context.go(AppRoutes.dishForm());
+          },
+          tooltip: 'Añadir Plato',
+          child: const Icon(Icons.add),
+        ),
+        body: _buildBody(viewmodel, context),
+      ),
+    );
+  }
+
+  Widget _buildBody(DishViewModel viewModel, BuildContext context) {
+    if (viewModel.errorMessage.isNotEmpty) {
+      return Center(child: Text(viewModel.errorMessage));
+    }
+
+    if (viewModel.dishes.isEmpty) {
+      return const Center(child: Text("No hay platos disponibles"));
+    }
+
+    return ListView.builder(
+      itemCount: viewModel.dishes.length,
+      itemBuilder: (context, index) {
+        final dish = viewModel.dishes[index];
+
+        return ListTile(
+          title: Text(dish.name ?? ''),
+          subtitle: Text(dish.category ?? ''),
+          trailing: Text(dish.price?.toStringAsFixed(2) ?? '-'),
+          onTap: () {
+            //context.go(AppRoutes.dishDetail(dish.id!));
+          },
+        );
+      },
+    );
+  }
+}
